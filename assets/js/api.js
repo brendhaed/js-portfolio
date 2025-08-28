5a99f455c6fe74d09dcd0deaf574a01a4e33c685
Brendha Eduarda Rodrigues <136868276+brendhaed@users.noreply.github.com>
2025-08-28 13:16:27 -0300

    json modificado foto

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,8 +1,7 @@
 
 async function fetchProfileData() {
-    const url = '';
+    const url = 'https://raw.githubusercontent.com/brendhaed/js-portfolio/refs/heads/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
 }
-// https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json
